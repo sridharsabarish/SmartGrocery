aa5028907f4a5cfcaba49d6f51ea24f1963a6ec5
Sabarish Sridhar <sabarish.sr@outlook.com>
2025-03-14 07:48:15 +0100

    Refactoring and support for visuals for monthly cost

--- groceryEstimator.py
+++ groceryEstimator.py
@@ -4,7 +4,7 @@ import glob
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def parseGroceryPDF(filename):
+def read_data_from_pdf(filename):
     pdfFileObj = open(filename, 'rb')
     pdfReader = PyPDF2.PdfReader(pdfFileObj)
     num_pages = len(pdfReader.pages)
@@ -27,22 +27,57 @@ def parseGroceryPDF(filename):
         return new_df
 
 
+def make_charts(combined_df):
+    # Plot a bar chart for item frequencies
+    plt.figure(figsize=(12, 8))
+    items_to_plot = combined_df[combined_df['Frequency'] >= 2]
+    plt.bar(items_to_plot['Item'], items_to_plot['Frequency'], color='skyblue')
+    plt.xlabel('Item')
+    plt.ylabel('Frequency')
+    plt.title('Frequency of Items Purchased (Frequency >= 2)')
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.savefig('outputs/item_frequencies.png')
 
 
-def process_grocery_data(month):
+    
+def make_pi_chart(top_10_items):
+    
+    plt.figure(figsize=(16, 16))
+    plt.pie(top_10_items['Total Cost'], labels=[f'{item}\n{cost:.2f} SEK' for item, cost in zip(top_10_items['Item'], top_10_items['Total Cost'])], autopct='%1.1f%%')
+    plt.title('Total Cost per Top 10 Items')
+    plt.legend(top_10_items['Item'], loc='upper left')
+    plt.savefig('outputs/top_10_items'+month+'.png')   
+    
+
+def read_monthly_grocery_data(month):
     all_df = pd.DataFrame()
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
     pd.set_option('display.width', None)
     pd.set_option('display.max_colwidth', None)
-
+    
+    total_cost=0.0;
     for filename in glob.glob(month+"/*.pdf"):
-        print(filename)
-        df = parseGroceryPDF(filename)
-        all_df = pd.concat([all_df, df], ignore_index=True)
+        #print(filename)
+       
+        df = read_data_from_pdf(filename)
         
-    print(all_df)
-    print(all_df.describe())
+        #print("Debugging here ...")
+        total_cost += df['Summa(SEK)'].astype(float).sum()
+       
+        
+        #print("Out of debug")
+        
+        # total_cost = df['Summa(SEK)'].sum()
+        # all_df_list = []
+        # all_df_list.append(total_cost)
+        # print(all_df_list)
+        all_df = pd.concat([all_df, df], ignore_index=True)
+    
+        
+    #print(all_df)
+    #print(all_df.describe())
     all_df['Summa(SEK)'] = all_df['Summa(SEK)'].astype(float)
     distinct_items = all_df['Beskrivning'].unique()
     item_costs = []
@@ -53,50 +88,81 @@ def process_grocery_data(month):
     item_costs_df.to_csv('outputs/grocery_data.csv', index=False)
     # Plot a pie chart
     top_10_items = item_costs_df.nlargest(20, 'Total Cost')
-    plt.figure(figsize=(16, 16))
+   
     other_items = item_costs_df.iloc[10:]
     other_items_cost = other_items['Total Cost'].sum()
     top_10_items.loc[len(top_10_items)] = ['Other', other_items_cost]
-    plt.pie(top_10_items['Total Cost'], labels=[f'{item}\n{cost:.2f} SEK' for item, cost in zip(top_10_items['Item'], top_10_items['Total Cost'])], autopct='%1.1f%%')
-    plt.title('Total Cost per Top 10 Items')
-    plt.legend(top_10_items['Item'], loc='upper left')
-    plt.savefig('outputs/top_10_items'+month+'.png')
-    return item_costs_df
+    #make_pi_chart(top_10_items)
+    print("Month: ",month," Total cost : ", total_cost)
+
+    return total_cost,item_costs_df
 
 import os
 #months = [f for f in os.listdir('.') if os.path.isdir(f)]
-months=["July","August","September","October","November","December", "January", "February"]
-a=[]
-for m in months:
-    print("Going through ", m)
-    a.append(process_grocery_data(m))
+
+def create_monthly_shopping_list():
+    # Todo : A template for what needs to be done.
+    print("TBD")
     
 
-combined_df = pd.concat(a, ignore_index=True)
-combined_df['Item'] = combined_df['Item'].str.replace('*', '')
-combined_df['Item'] = combined_df['Item'].str.replace(',', '')
-combined_df['Item'] = combined_df['Item'].str.lstrip()
-combined_df['Item'] = combined_df['Item'].apply(lambda x: x.split()[0])
-combined_df = combined_df.sort_values(by='Item', ascending=True)
-combined_df = combined_df[~combined_df['Item'].str.isdigit()]
 
+def cleanup_dataframe(combined_df):
+    combined_df['Item'] = combined_df['Item'].str.replace('*', '')
+    combined_df['Item'] = combined_df['Item'].str.replace(',', '')
+    combined_df['Item'] = combined_df['Item'].str.lstrip()
+    combined_df['Item'] = combined_df['Item'].apply(lambda x: x.split()[0])
+    combined_df = combined_df.sort_values(by='Item', ascending=True)
+    combined_df = combined_df[~combined_df['Item'].str.isdigit()]
 
-combined_df['Frequency'] = combined_df.groupby('Item')['Item'].transform('count')
+    combined_df['Frequency'] = combined_df.groupby('Item')['Item'].transform('count')
 
-combined_df = combined_df.drop_duplicates(subset='Item')
-combined_df = combined_df[['Item', 'Frequency']].sort_values(by='Frequency', ascending=False)
-print(combined_df)
+    combined_df = combined_df.drop_duplicates(subset='Item')
+    combined_df = combined_df[['Item', 'Frequency']].sort_values(by='Frequency', ascending=False)
+    return combined_df
 
+def main():
 
-combined_df.to_csv('outputs/combined_grocery_data.csv', index=False)
-top_10_items = combined_df.nlargest(10, 'Frequency')
-top_10_items.to_csv('outputs/top_ten_item.csv',index=False);
+    #Todo : Good to maybe create a folder called data and let the code figure out the names of the directory
+    months=["July","August","September","October","November","December", "January", "February"]
+    a=[]
+    b =[]
+    
+    for m in months:
+        #print("Going through ", m)
+        total_cost, new_df = read_monthly_grocery_data(m)
+        a.append(new_df)
+        b.append(total_cost);
+        
+        
+    combined_df = pd.concat(a, ignore_index=True)
+
+    
+    
+    combined_df = cleanup_dataframe(combined_df)
+    combined_df.to_csv('outputs/combined_grocery_data.csv', index=False)
+    top_10_items = combined_df.nlargest(10, 'Frequency')
+    top_10_items.to_csv('outputs/top_ten_item.csv',index=False);
+
+    #make_charts(combined_df)
+    
+    from matplotlib import pyplot as plt
+    plt.plot(months, b, marker='o', linestyle='-')
+    plt.xlabel('Months')
+    plt.ylabel('Total Cost')
+    plt.title('Groceries')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+    
+main()
+
 
 
 '''
 Todo :
 
 1. Connect the Df to a database and store the information?
-2. Make it easy to see the purchase trends?
-
+2. Make it easy to see the purchase trends? - Graph would be nice.
+3. Refactor
 '''
